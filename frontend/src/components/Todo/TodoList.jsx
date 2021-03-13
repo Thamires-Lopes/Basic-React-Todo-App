0761e384269e651d65fd675a9533ccf123524183
Thamires Lopes <thamiresl686@gmail.com>
2021-03-13 20:59:49 -0300

    Basic modal from edit todo done

--- frontend/src/components/Todo/TodoList.jsx
+++ frontend/src/components/Todo/TodoList.jsx
@@ -1,12 +1,15 @@
 /* eslint-disable no-underscore-dangle */
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Table, Button } from 'react-bootstrap';
 import { toast } from 'react-toastify';
 import { TodosContext } from '../../TodoContext';
 import myAxios from '../../utils/api';
+import TodoModal from './TodoEditModal';
 
 const TodoList = () => {
   const [todos, setTodos] = useContext(TodosContext);
+  const [modalShow, setModalShow] = useState(false);
+  const [todoToEdit, setTodoToEdit] = useState({});
 
   const onDeleteTodo = async (todo) => {
     try {
@@ -19,42 +22,62 @@ const TodoList = () => {
     }
   };
 
+  const setEditTodo = (todo) => {
+    setTodoToEdit(todo);
+    setModalShow(true);
+  };
+
   return (
-    <Table striped bordered hover variant="dark">
-      <thead>
-        <tr className="mb-2 text-center">
-          <th>#</th>
-          <th>Your todo</th>
-          <th>Options</th>
-        </tr>
-      </thead>
-      <tbody className="mb-2 text-center">
-        {todos.length ? todos.map((todo, index) => (
-          <tr key={todo._id}>
-            <td>
-              {index}
-            </td>
-            <td>
-              {todo.title}
-            </td>
-            <td>
-              <Button
-                onClick={() => onDeleteTodo(todo)}
-                variant="danger"
-              >
-                Delete
-              </Button>
-            </td>
+    <>
+      <TodoModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        todoToEdit={todoToEdit}
+      />
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr className="mb-2 text-center">
+            <th>#</th>
+            <th>Your todo</th>
+            <th>Options</th>
           </tr>
-        )) : (
-          <tr>
-            <td colSpan={3} align="center">
-              No Data Found
-            </td>
-          </tr>
-        )}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody className="mb-2 text-center">
+          {todos.length ? todos.map((todo, index) => (
+            <tr key={todo._id}>
+              <td>
+                {index}
+              </td>
+              <td>
+                {todo.title}
+              </td>
+              <td>
+                <Button
+                  className="mr-4"
+                  onClick={() => onDeleteTodo(todo)}
+                  variant="danger"
+                >
+                  Delete
+                </Button>
+
+                <Button
+                  onClick={() => setEditTodo(todo)}
+                  variant="primary"
+                >
+                  Edit
+                </Button>
+              </td>
+            </tr>
+          )) : (
+            <tr>
+              <td colSpan={3} align="center">
+                No Data Found
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
+    </>
   );
 };
 
