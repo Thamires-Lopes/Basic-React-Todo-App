65b870f5d9a9f740aefce79a7db762aa5e2db080
Thamires Lopes <thamiresl686@gmail.com>
2021-03-13 22:31:58 -0300

    Basic todo edit

--- backend/src/routes/todo.router.js
+++ backend/src/routes/todo.router.js
@@ -6,7 +6,7 @@ const router = Router();
 router.get("/todo", TodoController.index);
 router.post("/todo", TodoController.store);
 router.get("/todo/:id", TodoController.getOne);
-router.post("/todo/:id", TodoController.update);
+router.put("/todo/:id", TodoController.update);
 router.delete("/todo/:id", TodoController.remove);
 
 module.exports = router
--- frontend/src/components/Todo/TodoEditModal.jsx
+++ frontend/src/components/Todo/TodoEditModal.jsx
@@ -1,31 +1,70 @@
-import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+/* eslint-disable no-underscore-dangle */
+import React, { useContext, useState } from 'react';
+import {
+  Modal, Button, Form,
+} from 'react-bootstrap';
+import { toast } from 'react-toastify';
+import { TodosContext } from '../../TodoContext';
+import myAxios from '../../utils/api';
 
-const TodoModal = ({ show, onHide, todoToEdit }) => (
-  <Modal
-    show={show}
-    onHide={onHide}
-    size="lg"
-    aria-labelledby="contained-modal-title-vcenter"
-    centered
-  >
-    <Modal.Header closeButton>
-      <Modal.Title id="contained-modal-title-vcenter">
-        Edit Todo
-      </Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-      <h4>{todoToEdit.title}</h4>
-      <p>
-        Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-        dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-        consectetur ac, vestibulum at eros.
-      </p>
-    </Modal.Body>
-    <Modal.Footer>
-      <Button onClick={onHide}>Close</Button>
-    </Modal.Footer>
-  </Modal>
-);
+const TodoModal = ({ show, onHide, todoToEdit }) => {
+  const [todos, setTodos] = useContext(TodosContext);
+  const [edit, setEdit] = useState('');
+
+  const onSaveTodo = () => {
+    try {
+      const newTodos = todos.map((todo) => {
+        if (todo._id === todoToEdit._id) {
+          const data = {
+            ...todo,
+            title: edit,
+          };
+          myAxios.put(`/todo/${todoToEdit._id}`, { title: edit });
+          return data;
+        }
+        return todo;
+      });
+      setTodos(newTodos);
+      toast.info('Updated');
+    } catch (e) {
+      toast.error(e.message);
+    }
+    setEdit('');
+    onHide();
+  };
+
+  return (
+    <Modal
+      show={show}
+      onHide={onHide}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Edit Todo
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>{todoToEdit.title}</h4>
+        <Form>
+          <Form.Group>
+            <Form.Control
+              value={edit}
+              onChange={(event) => setEdit(event.target.value)}
+              type="text"
+              placeholder="Edit your todo"
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={onSaveTodo}>Save Todo</Button>
+        <Button onClick={onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
 
 export default TodoModal;
